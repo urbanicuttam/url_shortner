0a542324ca6203ca61c0c2d20c12cdda8acea613
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-01-11 21:19:55 +0530

    made changes in login.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { auth } from './firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import Login from './components/Login';
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { signInWithGoogle } from '../firebase.js';
 import GoogleButton from 'react-google-button'
+import BackgroundAnimate from './BackgroundAnimate.js';
 const Login = () => {
       
     return (
         <div>
             <center>
+                <h1>Hello! {localStorage.getItem("name")}</h1>
+                <p>Please Login to SHORTEN your link!</p>
                 <GoogleButton style={{"marginTop" : "200px"}} 
                 onClick={signInWithGoogle} />
             </center>
+            <BackgroundAnimate />
         </div>
     );
 }
